259f8da9c121a99a71c978b122eb0eb24f760fa3
xany <jgabaextra@gmail.com>
2023-09-14 11:06:52 +0200

    installAll

--- src/three/script.js
+++ src/three/script.js
@@ -2,18 +2,41 @@
 //Objects
 //Camera
 //Renderer
+import * as THREE from 'three';
+
 
 
 //Scene
 const scene = new THREE.Scene()
+
+//group
+const group = new THREE.Group()
 
 
 //MESH
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: "purple" })
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 1;
+// mesh.rotation.x=Math.PI * 0.25
 
-scene.add(mesh);
+
+
+//MESH 2
+const geometryT = new THREE.BoxGeometry(1, 1, 1)
+const materialT = new THREE.MeshBasicMaterial({ color: "green" })
+const meshT = new THREE.Mesh(geometryT, materialT);
+meshT.position.x = -1;
+
+//AxesHelper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+
+group.add(mesh,meshT);
+group.position.x=1;
+group.position.z=-2;
+scene.add(group);
 
 //Camera
 const aspect = {
@@ -33,4 +56,26 @@ const canvas = document.querySelector(".draw");
 const renderer = new THREE.WebGLRenderer({canvas});  // canvas: canvas  add webGLRenderer
 
 renderer.setSize(aspect.width,aspect.height)    //Renderer size
-renderer.render(scene,camera)      //display what camera see
+
+//Clock
+const clock = new THREE.Clock();
+
+const animate =()=>{
+   
+    //ElapsedTime
+    const elapsedTime = clock.getElapsedTime();
+    console.log(elapsedTime);
+
+    //update rotation on Y axis
+    group.rotation.y += 0.01 * Math.PI * 2;
+
+    //renderer
+    renderer.render(scene,camera)      //display what camera see
+    
+    //RequestAnimation
+    window.requestAnimationFrame(animate);
+}
+animate();
+
+//function will get called 60time per second on some devices 0.01 * 60 = 0.6 on Xaxis
+//function will get called 120time per second on some devices 0.01 * 120 = 1.2 on Xaxis
